52b1a0cefbc699af9c900c9a96479020b2976115
trung1204 <https://github.com/trung1204/DuAN1.git>
2023-11-15 23:33:06 +0700

    giaodien

--- view/footer.php
+++ view/footer.php
@@ -1,4 +1,158 @@
-<footer>
+<!-- post -->
+<section class="posts">
+        <div class="container">
+            <header class="row posts__title">
+                <div class="posts__title-wrap col-lg-12 col-md-12 col-sm-12">
+                    <h2 class="posts__heading">
+                        Tin tức - Bài viết
+                    </h2>
+                </div>
+            </header>
+            <article class="row posts__list">
+                <div class="posts__item col-lg-3 col-md-6 col-sm-6">
+                    <div class="posts__item-img" >
+                        <img src="images1/post/got-it-e-voucher.png" width="80%">
+                    </div>
+                    <h3 class="posts__item-heading">
+                        <a href="" class="posts__item-title">
+                            Mẹo săn mã voucher giảm giá tại Edu Book
+                        </a>
+                    </h3>
+                    <div class="posts__item-date">
+                        28/05/2020
+                    </div>
+                    <div class="posts__item-cate-wrap">
+                        <div class="posts__item-cate">
+                            <i class="fa fa-folder posts__item-cate-icon"></i>
+                            <div class="posts__item-cate-name">
+                                Tin tức
+                            </div>
+                        </div>
+                        <div class="posts__item-cate-ad">
+                            <i class="fa fa-user posts__item-cate-ad-icon"></i>
+                            <div class="posts__item-cate-ad-name">
+                                Nguyễn Nhung
+                            </div>   
+                        </div>
+                    </div>
+                    <div class="posts__item-description">
+                        Nếu bạn đang gặp phải những vấn đề về săn, lấy, sử dụng mã voucher của EduBook. Xin hãy yên tâm, bài viết này 
+                        dành cho bạn! Trong bài viết này mình sẽ chia sẻ đến bạn những mẹo, những kinh nghiệm giúp bạn săn được nhiều 
+                        mã giảm giá EduBook nhất có thể.....
+                    </div>
+                </div>
+
+                <div class="posts__item col-lg-3 col-md-6 col-sm-6">
+                    <div class="posts__item-img">
+                        <img src="images1/post/hinh-ghep-15986994500641611959184.jpg">
+                    </div>
+                    <h3 class="posts__item-heading">
+                        <a href="" class="posts__item-title">
+                            Cách phân biệt sách thật, giả chính xác
+                        </a>
+                    </h3>
+                    <div class="posts__item-date">
+                        05/06/2020
+                    </div>
+                    <div class="posts__item-cate-wrap">
+                        <div class="posts__item-cate">
+                            <i class="fa fa-folder posts__item-cate-icon"></i>
+                            <div class="posts__item-cate-name">
+                                Tin tức
+                            </div>
+                        </div>
+                        <div class="posts__item-cate-ad">
+                            <i class="fa fa-user posts__item-cate-ad-icon"></i>
+                            <div class="posts__item-cate-ad-name">
+                                Trung Trần
+                            </div>   
+                        </div>
+                    </div>
+                    <div class="posts__item-description">
+                        Để các độc giả, phụ huynh, học sinh và các thầy cô giáo… không mua phải các loại sách tham khảo giả, sách in lậu, 
+                        mình sẽ hướng dẫn cách phân biệt sách thật, giả nhanh chóng và chính xác. Nếu bạn chú ý quan sát một xíu là có thể 
+                        phân biệt được sách giả ngay. Với sách giả khi cầm trên tay sẽ có cảm giác mềm hơn, không cứng và chắc tay như sách thật...
+                    </div>
+                </div>
+
+                <div class="posts__item col-lg-3 col-md-6 col-sm-6">
+                    <div class="posts__item-img">
+                        <img src="images1/post/cach-su-dung-ma-giam-gia-hieu-qua-tai-vnreviews.png">
+                    </div>
+                    <h3 class="posts__item-heading">
+                        <a href="#" class="posts__item-title">
+                            Sử dụng mã giảm giá Edu Book như thế nào
+                        </a>
+                    </h3>
+                    <div class="posts__item-date">
+                        26/05/2020
+                    </div>
+                    <div class="posts__item-cate-wrap">
+                        <div class="posts__item-cate">
+                            <i class="fa fa-folder posts__item-cate-icon"></i>
+                            <div class="posts__item-cate-name">
+                                Tin tức
+                            </div>
+                        </div>
+                        <div class="posts__item-cate-ad">
+                            <i class="fa fa-user posts__item-cate-ad-icon"></i>
+                            <div class="posts__item-cate-ad-name">
+                                Tùng Lương
+                            </div>   
+                        </div>
+                    </div>
+                    <div class="posts__item-description">
+                        Sau khi lấy được mã rồi thì tất nhiên bạn phải biết cách sử dụng nó rồi. Về cơ bản thì EduBook cho phép bạn sử dụng cùng 
+                        lúc 3 loại mã giảm giá mà mình đã kể trên trong cùng 1 đơn hàng. Bao gồm:
+                            Mã FreeShip,
+                            Mã giảm giá của Shop,
+                            Và Ưu đãi từ đối tác thanh toán,....
+                    </div>
+                </div>
+
+                <div class="posts__item col-lg-3 col-md-6 col-sm-6">
+                    <div class="posts__item-img">
+                        <img src="images1/post/images.jfif">
+                    </div>
+                    <h3 class="posts__item-heading">
+                        <a href="" class="posts__item-title">
+                            Hướng dẫn đổi trả sách, hoàn tiền Edu Book
+                        </a>
+                    </h3>
+                    <div class="posts__item-date">
+                        22/05/2020
+                    </div>
+                    <div class="posts__item-cate-wrap">
+                        <div class="posts__item-cate">
+                            <i class="fa fa-folder posts__item-cate-icon"></i>
+                            <div class="posts__item-cate-name">
+                                Tin tức
+                            </div>
+                        </div>
+                        <div class="posts__item-cate-ad">
+                            <i class="fa fa-user posts__item-cate-ad-icon"></i>
+                            <div class="posts__item-cate-ad-name">
+                                Nguyễn Nhung
+                            </div>   
+                        </div>
+                    </div>
+                    <div class="posts__item-description">
+                        Mua hàng trên Edu Book nhưng sản phẩm bạn nhận được không giống hình, sản phẩm bị lỗi. Bạn nghĩ mình bị lừa, bạn bối rối không biết
+                        làm thế nào và quyết định search google: “Phải làm gì khi muốn hoàn trả hàng hoàn tiền trên Edu Book”.  Bài viết này sẽ hướng dẫn 
+                        bạn đổi trả sách EduBook đúng quy trình...
+                    </div>
+                </div>
+            </article>
+            <article class="row posts__view">
+                <a href="post.html" class="posts__view-btn">Xem thêm</a>
+            </article>
+        </div>
+    </section>
+    <!-- end post -->
+
+
+    <!-- footer -->
+    <footer>
         <section class="footer__top">
             <div class="container">
                 <div class="row">
--- view/header.php
+++ view/header.php
@@ -45,7 +45,10 @@
                 </section>
             </div>
         </div>
-		<div class="header__bottom">
+        <!--end header top -->
+
+        <!-- header bottom -->
+        <div class="header__bottom">
             <div class="container">
                 <section class="row">
                     <div class="col-lg-3 col-md-4 col-sm-12 header__logo">
@@ -96,3 +99,115 @@
             </div>   
         </div>
         <!--end header bottom -->
+
+        <!-- header nav -->
+        <div class="header__nav">
+            <div class="container">
+                <section class="row">
+                    <div class="header__nav-menu-wrap col-lg-3 col-md-0 col-sm-0">
+                        <i class="fas fa-bars header__nav-menu-icon"></i>
+                        <div class="header__nav-menu-title">Danh mục sản phẩm</div>
+                    </div>
+
+                    <div class="header__nav col-lg-9 col-md-0 col-sm-0">
+                        <ul class="header__nav-list">
+                            <li class="header__nav-item">
+                                <a href="index.html" class="header__nav-link">Trang chủ</a>
+                            </li>
+                            <li class="header__nav-item">
+                                <a href="category.html" class="header__nav-link">Danh mục sản phẩm</a>
+                            </li>
+                            <li class="header__nav-item">
+                                <a href="product.html" class="header__nav-link">Sản phẩm</a>
+                            </li>
+                            <li class="header__nav-item">
+                                <a href="post.html" class="header__nav-link">Bài viết</a>
+                            </li>
+                            <li class="header__nav-item">
+                                <a href="#" class="header__nav-link">Tuyển cộng tác viên</a>
+                            </li>
+                            <li class="header__nav-item">
+                                <a href="contact.html" class="header__nav-link">Liên hệ</a>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </header>
+    <!--end header nav -->
+
+    <!-- slide - menu list -->
+    <section class="menu-slide">
+        <div class="container">
+            <div class="row">
+                <nav class="menu__nav col-lg-3 col-md-12 col-sm-0">
+                    <ul class="menu__list">
+                        <li class="menu__item menu__item--active">
+                            <a href="#" class="menu__link">
+                            <img src="images1/item/baby-boy.png" alt=""  class="menu__item-icon" id="Capa_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512">
+                            Sách Tiếng Việt</a>
+                        </li>
+                        <li class="menu__item">
+                            <a href="#" class="menu__link">
+                            <img src="images1/item/translation.png" alt="" class="menu__item-icon" id="Capa_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512">
+                            Sách nước ngoài</a>
+                        </li>
+                      
+                        <li class="menu__item">
+                            <a href="#" class="menu__link">
+                                <img src="images1/item/1380754_batman_comic_hero_superhero_icon.png" alt="" class="menu__item-icon"  viewBox="0 0 512 512" width="1012" height="512">
+
+                            Manga - Comic</a>
+                        </li>
+                      
+                    </ul>
+                </nav>
+
+                <div class="slider col-lg-9 col-md-12 col-sm-0">
+                    <div class="row">
+                        <div class="slide__left col-lg-8 col-md-0 col-sm-0">
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="3000">
+                                <!-- <ol class="carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                </ol> -->
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img src="images1/banner/363488_final1511.jpg" class="d-block w-100" alt="...">
+                                    </div>
+                                    <div class="carousel-item">
+                                      <img src="images1/banner/Gold_DongA_600X350.jpg" class="d-block w-100" alt="...">
+                                    </div>
+                                    <div class="carousel-item">
+                                      <img src="images1/banner/megabook-glod600X350.png" class="d-block w-100" alt="...">
+                                    </div>
+                                </div>
+                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+                            <div class="slide__left-bottom">
+                                <div class="slide__left-botom-one">
+                                    <img src="images1/banner/363107_05.jpg" class="slide__left-bottom-one-img">
+                                </div>
+                                <div class="slide__left-bottom-two">
+                                    <img src="images1/banner/363104_06.jpg" class="slide__left-bottom-tow-img">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="slide__right col-lg-4 col-md-0 col-sm-0">
+                            <img src="images1/banner/slider-right.png" class="slide__right-img">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
