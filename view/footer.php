bbd228be9f54e255220651217e47d275edab19b6
trung1204 <https://github.com/trung1204/DuAN1.git>
2023-11-15 21:25:32 +0700

    header

--- view/footer.php
+++ view/footer.php
@@ -1,37 +1,125 @@
-<div id="footer" class="shell">
-		<div class="top">
-			<div class="cnt">
-				<div class="col about">
-					<h4>About BestSellers</h4>
-					<p>Nulla porttitor pretium mattis. Mauris lorem massa, ultricies non mattis bibendum, semper ut erat. Morbi vulputate placerat ligula. Fusce <br />convallis, nisl a pellentesque viverra, ipsum leo sodales sapien, vitae egestas dolor nisl eu tortor. Etiam ut elit vitae nisl tempor tincidunt. Nunc sed elementum est. Phasellus sodales viverra mauris nec dictum. Fusce a leo libero. Cras accumsan enim nec massa semper eu hendrerit nisl faucibus. Sed lectus ligula, consequat eget bibendum eu, consequat nec nisl. In sed consequat elit. Praesent nec iaculis sapien. <br />Curabitur gravida pretium tincidunt.  </p>
-				</div>
-				<div class="col store">
-					<h4>Store</h4>
-					<ul>
-						<li><a href="#">Home</a></li>
-						<li><a href="#">Special Offers</a></li>
-						<li><a href="#">Log In</a></li>
-						<li><a href="#">Account</a></li>
-						<li><a href="#">Basket</a></li>
-						<li><a href="#">Checkout</a></li>
-					</ul>
-				</div>
-				<div class="col" id="newsletter">
-					<h4>Newsletter</h4>
-					<p>Lorem ipsum dolor sit amet  consectetur. </p>
-					<form action="" method="post">
-						<input type="text" class="field" value="Your Name" title="Your Name" />
-						<input type="text" class="field" value="Email" title="Email" />
-						<div class="form-buttons"><input type="submit" value="Submit" class="submit-btn" /></div>
-					</form>
-				</div>
-				<div class="cl">&nbsp;</div>
-				<div class="copy">
-					<p>&copy; <a href="#">BestSeller.com</a>. Design by <a href="http://css-free-templates.com/">CSS-FREE-TEMPLATES.COM</a></p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- End Footer -->
+<footer>
+        <section class="footer__top">
+            <div class="container">
+                <div class="row">
+                    <article class="footer__top-intro col-lg-5 col-md-4 col-sm-6">
+                        <h4 class="footer__top-intro-heading">
+                            Về chúng tôi
+                        </h4>
+                        <div class="footer__top-intro-content">
+                            Edubook là cửa hàng luôn cung cấp cho các bạn tìm tòi tri thức, đam mê 
+                            đọc sách trên khắp cả nước.Chúng tôi sẽ liên tục cập 
+                            nhật những cuốn sách hay nhất, mới nhất, chất lượng nhất 
+                            giúp người đọc có những cuốn sách hay nhất để đọc! <br> <br>
+                            Điện thoại: 0352 860 701 <br>
+                            Email: teamed2@gmail.com <br>
+                            Zalo:  039.882.3232 <br>
+                        </div>
+                    </article>
+
+                    <article class="footer__top-policy col-lg-3 col-md-4 col-sm-6">
+                        <h4 class="footer__top-policy-heading">
+                            Chính sách mua hàng
+                        </h4>
+
+                        <ul class="footer__top-policy-list">
+                            <li class="footer__top-policy-item">
+                                <a href="#" class="footer__top-policy-link">Hình thức đặt hàng</a>
+                            </li>
+                            <li class="footer__top-policy-item">
+                                <a href="#" class="footer__top-policy-link">Hình thức thanh toán</a>
+                            </li>
+                            <li class="footer__top-policy-item">
+                                <a href="#" class="footer__top-policy-link">Phương thức vận chuyển</a>
+                            </li>
+                            <li class="footer__top-policy-item">
+                                <a href="#" class="footer__top-policy-link">Chính sách đổi trả</a>
+                            </li>
+                            <li class="footer__top-policy-item">
+                                <a href="#" class="footer__top-policy-link">Hướng dẫn sử dụng</a>
+                            </li>
+                        </ul>
+                    </article>
+
+                    <article class="footer__top-contact-wrap col-lg-4 col-md-4 col-sm-6">
+                        <h4 class="footer__top-contact-heading">
+                            Hotline liên hệ
+                        </h4>
+
+                        <div class="footer__top-contact">
+                            <div class="footer__top-contact-icon">
+                                <img src="images/phone_top.png" class="footer__top-contact-img">
+                            </div>
+
+                            <div class="footer__top-contact-phone-wrap">
+                                <div class="footer__top-contact-phone">
+                                    039.882.3232
+                                </div>
+                                <div class="footer__top-contact-des">
+                                    (Giải đáp thắc mắc 24/24)
+                                </div>
+                            </div>
+                        </div>
+                    
+                        <h4 class="footer__top-contact-heading">
+                            Kết nối với chúng tôi
+                        </h4>
+
+                        <div class="footer__top-contact-social">
+                            <a href="#" class="footer__top-contact-social-link">
+                                <img src="images/facebook.png">
+                            </a>
+                            <a href="#" class="footer__top-contact-social-link">
+                                <img src="images/youtube.png">
+                            </a>
+                            <a href="#" class="footer__top-contact-social-link">
+                                <img src="images/tiktok.png">
+                            </a>
+                            <a href="#" class="footer__top-contact-social-link">
+                                <img src="images/zalo.png">
+                            </a>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </section>
+        <section class="footer__bottom">
+            <div class="container">
+                <div class="row">
+                    <span class="footer__bottom-content">@Bản quyền thuộc về edubook | Thiết kế bởi team ED2 </span>
+                </div>
+            </div>
+        </section>
+    </footer>
+    <!-- end footer -->
+
+    <div class="main__modal">
+        <div class="modal__overlay"></div>
+        <div class="modal__body">
+            <!-- resgist form -->
+            <div class="sale-off">
+                <div class="sale-off__container">
+                    <h2 class="sale-off__heading">
+                        Nhận phiếu giảm giá <span class="sale-off__sp">30%</span>  khi mua <br> <span class="sale-off__name">Học tập qua dự án</span> 
+                    </h2>
+                    <div class="sale-off__img">
+                        <img src="images1/product/hoc-tap-qua-du-an-2-01-1.jpg" width="300">
+                    </div>
+                    
+                    <a href="product.html" class="sale-off__link">
+                        <button class="sale-off__btn">Mua ngay</button>
+                    </a>
+                </div>
+
+                <div class="sale-off__close">
+                    <!-- <svg class="sale-off__close-icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg> -->
+                    x
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <script src="js/jq.js"></script>
+    <script src="js/index.js"></script>
 </body>
 </html>
--- view/header.php
+++ view/header.php
@@ -1,100 +1,98 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title>CSS Free Templates with jQuery Slider</title>
-	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-	<link rel="shortcut icon" href="./view/css/images/favicon.ico" />
-	<link rel="stylesheet" href="./view/css/style.css" type="text/css" media="all" />
-	<script type="text/javascript" src="view/js/jquery-1.6.2.min.js"></script>
-	<script type="text/javascript" src="view/js/jquery.jcarousel.min.js"></script>
-	<!--[if IE 6]>
-		<script type="text/javascript" src="js/png-fix.js"></script>
-	<![endif]-->
-	<script type="text/javascript" src="view/js/functions.js"></script>
-	
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ - EduBook</title>
+    <script src="js/jquery-3.3.1.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/fontawesome/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Paytone+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/home.css">
 </head>
 <body>
-	<!-- Header -->
-	<div id="header" class="shell">
-		<div id="logo"><h1><a href="#">BestSeller</a></h1><span><a href="#">free css template</a></span></div>
-		<!-- Navigation -->
-		<div id="navigation">
-			<ul>
-				<li><a href="index.php" class="active">Home</a></li>
-				<li><a href="index.php?act=gioithieu">Products</a></li>
-				<li><a href="index.php?act=lienhe">Promotions</a></li>
-				<li><a href="index.php?act=gioithieu">Profile</a></li>
-				<li><a href="index.php?act=gopy">About Us</a></li>
-				<li><a href="index.php?act=hoidap">Contacts</a></li>
-			</ul>
-		</div>
-		<!-- End Navigation -->
-		<div class="cl">&nbsp;</div>
-		<!-- Login-details -->
-		<div id="login-details">
-			<p>Welcome, <a href="#" id="user">Guest</a> .</p><p><a href="#" class="cart" ><img src="./view/css/images/cart-icon.png" alt="" /></a>Shopping Cart (0) <a href="#" class="sum">$0.00</a></p>
-		</div>
-		<!-- End Login-details -->
-	</div>
-	<!-- End Header -->
-	<!-- Slider -->
-	<div id="slider">
-		<div class="shell">
-			<ul>
-				<li>
-					<div class="image">
-						<img src="./view/css/images/books.png" alt="" />
-					</div>
-					<div class="details">
-						<h2>Bestsellers</h2>
-						<h3>Special Offers</h3>
-						<p class="title">Pellentesque congue lorem quis massa blandit non pretium nisi pharetra</p>
-						<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id odio in tortor scelerisque dictum. Phasellus varius sem sit amet metus volutpat vel vehicula nunc lacinia.</p>
-						<a href="#" class="read-more-btn">Read More</a>
-					</div>
-				</li>
-				<li>
-					<div class="image">
-						<img src="./view/css/images/books.png" alt="" />
-					</div>
-					<div class="details">
-						<h2>Bestsellers</h2>
-						<h3>Special Offers</h3>
-						<p class="title">Pellentesque congue lorem quis massa blandit non pretium nisi pharetra</p>
-						<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id odio in tortor scelerisque dictum. Phasellus varius sem sit amet metus volutpat vel vehicula nunc lacinia.</p>
-						<a href="#" class="read-more-btn">Read More</a>
-					</div>
-				</li>
-				<li>
-					<div class="image">
-						<img src="./view/css/images/books.png" alt="" />
-					</div>
-					<div class="details">
-						<h2>Bestsellers</h2>
-						<h3>Special Offers</h3>
-						<p class="title">Pellentesque congue lorem quis massa blandit non pretium nisi pharetra</p>
-						<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id odio in tortor scelerisque dictum. Phasellus varius sem sit amet metus volutpat vel vehicula nunc lacinia.</p>
-						<a href="#" class="read-more-btn">Read More</a>
-					</div>
-				</li>
-				<li>
-					<div class="image">
-						<img src="./view/css/images/books.png" alt="" />
-					</div>
-					<div class="details">
-						<h2>Bestsellers</h2>
-						<h3>Special Offers</h3>
-						<p class="title">Pellentesque congue lorem quis massa blandit non pretium nisi pharetra</p>
-						<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id odio in tortor scelerisque dictum. Phasellus varius sem sit amet metus volutpat vel vehicula nunc lacinia.</p>
-						<a href="#" class="read-more-btn">Read More</a>
-					</div>
-				</li>
-			</ul>
-			<div class="nav">
-				<a href="#">1</a>
-				<a href="#">2</a>
-				<a href="#">3</a>
-				<a href="#">4</a>
-			</div>
-		</div>
-	</div>
+    <div class="app">
+        <header id="header">
+        <!-- header top -->
+        <div class="header__top">
+            <div class="container">
+                <section class="row flex">
+                    <div class="col-lg-5 col-md-0 col-sm-0 heade__top-left">
+                        <span>EduBook - Cội nguồn của tri thức</span>
+                    </div>
+
+                    <nav class="col-lg-7 col-md-0 col-sm-0 header__top-right">
+                        <ul class="header__top-list">
+                            <li class="header__top-item">
+                                <a href="#" class="header__top-link">
+
+                                Hỏi đáp</a>
+                            </li>
+                            <li class="header__top-item">
+                                <a href="#" class="header__top-link">Hướng dẫn</a>
+                            </li>
+                            <li class="header__top-item">
+                                <a href="#" class="header__top-link">Đăng ký</a>
+                            </li>
+                            <li class="header__top-item">
+                                <a href="#" class="header__top-link">Đăng nhập</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </section>
+            </div>
+        </div>
+		<div class="header__bottom">
+            <div class="container">
+                <section class="row">
+                    <div class="col-lg-3 col-md-4 col-sm-12 header__logo">
+                        <h1 class="header__heading">
+                            <a href="#" class="header__logo-link">
+                                <img src="images1/logo1.png" alt="Logo" class="header__logo-img">
+                            </a>
+                        </h1>
+                    </div>
+
+                    <div class="col-lg-6 col-md-7 col-sm-0 header__search">
+                        <select name="" id="" class="header__search-select">
+                            <option value="0">All</option>
+                            <option value="1">Sách tiếng việt</option>
+                            <option value="2">Sách sách nước ngoài</option>
+                            <option value="3">Manga-Comic</option>
+                            
+                        </select>
+                        <input type="text" class="header__search-input" placeholder="Tìm kiếm tại đây...">
+                        <button class="header__search-btn">
+                            <div class="header__search-icon-wrap">
+                                <i class="fas fa-search header__search-icon"></i>
+                            </div>
+                        </button>
+                    </div>
+
+                    <div class="col-lg-2 col-md-0 col-sm-0 header__call">
+                        <div class="header__call-icon-wrap">
+                            <i class="fas fa-phone-alt header__call-icon"></i>  
+                        </div>
+                        <div class="header__call-info">
+                            <div class="header__call-text">
+                                Gọi điện tư vấn
+                            </div>
+                            <div class="header__call-number">
+                                039.882.3232
+                            </div>
+                        </div>
+                    </div>
+
+                    <a href="cart.html" class="col-lg-1 col-md-1 col-sm-0 header__cart">
+                        <div class="header__cart-icon-wrap">
+                            <span class="header__notice">4</span>
+                            <i class="fas fa-shopping-cart header__nav-cart-icon"></i>
+                        </div>
+                    </a>
+                </section>
+            </div>   
+        </div>
+        <!--end header bottom -->
