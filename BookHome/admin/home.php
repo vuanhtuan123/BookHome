ebbc706dfec58b0873a508a7dbd43e7098b46458
trung1204 <https://github.com/trung1204/DuAN1.git>
2023-11-17 17:18:31 +0700

    update san pham php

---
+++ BookHome/admin/home.php
@@ -0,0 +1,31 @@
+<table class="table table-bordered " id="myTable">
+            <thead class="mt80">
+                <tr class="">
+                    <th width="auto">Tên Nhân Viên</th>
+                    <th width="auto">Giới Tính</th>
+                    <th>Ngày Sinh</th>
+                    <th>Địa Chỉ</th>
+                    <th>Chức Vụ</th>
+                    <th width="5px; !important">Tính Năng</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    foreach($listdanhmuc as $danhmuc){
+                        extract($danhmuc)
+                        echo ' <tr>
+                        <td>'.$id.'</td>
+                        <td>'.$name.'</td>
+                        <td>
+                            <a class="add" title="Lưu Lại" data-toggle="tooltip"><i class="fa fa-floppy-o"
+                                    aria-hidden="true"></i></a>
+                            <a class="edit" title="Sửa" data-toggle="tooltip"><i class="fa fa-pencil"
+                                    aria-hidden="true"></i></a>
+                            <a class="delete" title="Xóa" data-toggle="tooltip"><i class="fa fa-trash-o"
+                                    aria-hidden="true"></i></a>
+                        </td>
+                    </tr>';
+                    }
+                ?>
+            </tbody>
+</table>
---
+++ BookHome/index.php
@@ -0,0 +1,26 @@
+<?php 
+    include "model/pdo.php";
+    include "model/sanpham.php";
+    include "view/header.php";
+    include "global.php";
+
+    $spnew=loadall_sanpham_home();
+
+    if((isset($_GET['act']))&&($_GET['act']!="")){
+        $act=$_GET['act'];
+        switch ($act) {
+            case 'gioithieu':
+                include "view/gioithieu.php";
+                break;
+                case 'lienhe':
+                    include "view/lienhe.php";
+                    break;
+            default:
+                include "view/home.php";
+                break;
+        }
+    }else{
+        include "view/home.php";
+    }
+    include "view/footer.php";
+?>
